cddd33ca7cb44bc9b8a3d83c84f20198fa431ac3
Colin <clinehan4880@icloud.com>
2023-03-13 18:35:01 -0400

    Lab3 v1

---
+++ Lab3/test/LikertQuestionTest.java
@@ -0,0 +1,70 @@
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class LikertQuestionTest {
+  private String longRandom;
+  public LikertQuestionTest() {
+    longRandom = "aosdifjaso oifhas;ldihv;al skdfha;osidghv;osiadhvbasdjkhvn";
+  }
+
+  @Test
+  public void testCreateValidLikertQuestion() {
+    Random r = new Random(200);
+    for (int i=0;i<1000;i++) {
+      int start = r.nextInt(longRandom.length()-1);
+      int end = start + r.nextInt(longRandom.length()-start-1) + 1;
+      String questionText = longRandom.substring(start,end);
+      Question q = new LikertQuestion(questionText+"?");
+      assertEquals(questionText+"?",q.getQuestionText());
+      assertEquals("Likert",q.getType());
+    }
+
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testCreateLikertQuestionNoText() {
+    new LikertQuestion("");
+  }
+
+
+
+  @Test
+  public void testAnswerCorrectly() {
+    String []answers = {"strongly agree",
+                        "agree",
+                        "neither agree nor disagree",
+                        "disagree",
+                        "strongly disagree"};
+    for (String answer:answers) {
+      Question q = new LikertQuestion("Is this a trick question?");
+      assertFalse(q.hasBeenAnswered());
+
+      q.answer(answer);
+      assertEquals(answer.toLowerCase(), q.getEnteredAnswer());
+      assertTrue(q.hasBeenAnswered());
+    }
+  }
+
+  @Test
+  public void testAnswerInCorrectly() {
+    String []answers = {"weakly disagree",""};
+    for (String answer:answers) {
+      Question q = new LikertQuestion("Is this a trick question?");
+      assertFalse(q.hasBeenAnswered());
+
+      try {
+        q.answer(answer);
+        fail("Likert question accepted an invalid answer");
+      }
+      catch (IllegalArgumentException e) {
+        assertFalse(q.hasBeenAnswered());
+      }
+    }
+  }
+
+
+}
